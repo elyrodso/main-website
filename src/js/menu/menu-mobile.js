9033fe8b8e5d83d12b942cebe48f4d5c5897dbaa
KorbsStudio <KorbsStudio@OnMail.com>
2021-07-13 14:56:02 -0400

    Mobile support

---
+++ src/js/menu/menu-mobile.js
@@ -0,0 +1,7 @@
+function openMobileMenu() {
+    document.getElementById('mm').style.display = 'inherit';
+}
+
+function closeMobileMenu() {
+    document.getElementById('mm').style.display = 'none';
+}
