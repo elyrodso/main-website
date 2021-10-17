9730978d20300ad001c63849d54d8080eeb6852e
KorbsStudio <KorbsStudio@OnMail.com>
2021-10-17 17:21:30 -0400

    Remove unused JavaScript

--- assets/js/menu/menu-mobile.js
+++
@@ -1,7 +0,0 @@
-function openMobileMenu() {
-    document.getElementById('mm').style.display = 'inherit';
-}
-
-function closeMobileMenu() {
-    document.getElementById('mm').style.display = 'none';
-}
--- assets/js/menu/menu-with-hero.js
+++
@@ -1,7 +0,0 @@
-const event11 = window.addEventListener('scroll', function(e) {
-    if(this.scrollY < 530) {
-        document.getElementById('h_').style.background = 'transparent';
-    } else {
-        document.getElementById('h_').style.background = '#0B1725';
-    }
-});
